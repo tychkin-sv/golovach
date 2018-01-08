a37d4dd31984b279a25c33f0ee5104482692c44e
Sergii Tychkin <sergiy.tychkin@gmail.com>
2018-01-08 21:04:32 +0200

    change out

--- src/main/java/ua/tsv/_08_multithreading/_0_thread_runnable/ThreadExample_1B.java
+++ src/main/java/ua/tsv/_08_multithreading/_0_thread_runnable/ThreadExample_1B.java
@@ -5,14 +5,14 @@ package ua.tsv._08_multithreading._0_thread_runnable;
  */
 public class ThreadExample_1B {
     public static void main(String[] args) throws InterruptedException {
-    PrintRunnable runnable = new PrintRunnable("B", 1000);
+    PrintRunnable runnable = new PrintRunnable(" B", 1000);
     Thread thread = new Thread(runnable);
     // Забыли запустить поток
     // thread.start();
 
     for (int k = 0; k < 10; k++) {
         Thread.sleep(250);
-        System.out.println("ThreadExample_1B A k = " + k);
+        System.out.println("A");
     }
 
 
